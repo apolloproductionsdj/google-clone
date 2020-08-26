9f9f05cb1db744b8a181cfcd097d6850e2ee158f
apolloproductionsdj <apolloproductiosdj@gmail.com>
2020-08-25 17:15:04 -0700

    finshed with <Search />

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,11 @@ import "./Home.css";
 import { Link } from 'react-router-dom';
 import AppsIcon from '@material-ui/icons/Apps';
 import { Avatar } from '@material-ui/core';
+import Search from './Search';
 
 function Home() {
   return (
     <div className="home">
-      <h1>This is the Home Page</h1>
-      {/* Header */}
       <div className="home__header">
         <div className="home__headerLeft">
           <Link to='about'>About</Link>
@@ -25,7 +24,13 @@ function Home() {
 
       {/* Body */}
       <div className="home__body">
-        
+        <img 
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" 
+          alt=""
+        />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
       </div>  
     </div>
   )

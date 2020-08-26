f2806acb7781eed267b096492f005fa3fb2c22c6
apolloproductionsdj <apolloproductiosdj@gmail.com>
2020-08-25 17:36:26 -0700

    pulled value out of input field by changing the div that surrounds the input to form and adding type=submit to button

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -7,24 +7,24 @@ import { Button } from '@material-ui/core';
 
 
 function Search() {
-  // const [input, setInput] = useState('');
+  const [input, setInput] = useState('');
 
-  // const search = e => {
-  //   e.preventDefault();
-
+  const search = e => {
+    e.preventDefault();
+    console.log('You hit the search button >>', input);
   }
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__Input">
         <SearchIcon className="search__inputIcon" />
-        <input />
+        <input value={input} onChange={e => setInput(e.target.value)}/>
         <MicIcon />
       </div>
       <div className="search__buttons">
-        <Button onClick={search} variant="outlined">Google Search</Button>
+        <Button type="submit" onClick={search} variant="outlined">Google Search</Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
-    </div>
+    </form>
   )
 }
 
